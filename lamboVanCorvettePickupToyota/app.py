38aecfc59c69678c1570f16d9d05c698283c6e94
Noah Turcas <noahturc@gmail.com>
2025-01-09 14:31:22 -0500

    1/9/2025 2:31

--- lamboVanCorvettePickupToyota/app.py
+++ lamboVanCorvettePickupToyota/app.py
@@ -184,4 +184,5 @@ Up to 69.8 cubic feet with the rear seats folded.'''
     return x
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
