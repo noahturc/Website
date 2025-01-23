9538aabad80231aaa6ee7d85a0ddee13181f037f
Noah Turcas <noahturc@gmail.com>
2025-01-23 15:06:14 -0500

    minor change

--- lamboVanCorvettePickupToyota/app.py
+++ lamboVanCorvettePickupToyota/app.py
@@ -5,6 +5,17 @@ import os
 
 app = Flask(__name__)
 
+
+
+#########################
+# from chatgpt to stop error on aws
+@app.before_request
+def log_request_info():
+    print(f"Request: {request.method} {request.path}")
+#########################
+
+
+
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 from werkzeug.middleware.proxy_fix import ProxyFix
